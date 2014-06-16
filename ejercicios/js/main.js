7b21f8e576d8ca66cccae20098ab8bec3f05bdcf
David Velásquez <zero.blend@gmail.com>
2014-06-16 16:42:30 -0500

    Ejercicio 1

--- ejercicios/js/main.js
+++ ejercicios/js/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-  
+  // Ejercicios JS
+
+  // jQuery
+  $('.btn-send').click(function(event) {
+    event.preventDefault();
+    var userName = $('.user-name').val();
+    $('.hello').text(userName);
+  });
+
 });
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = Date();
+}
